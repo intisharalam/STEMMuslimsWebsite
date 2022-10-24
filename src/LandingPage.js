85e3729c6b91d4f079615285b18af392a89a2fc4
root <root@MohamedSharif>
2022-10-24 10:31:30 +0100

    Fixed logo position and added font awesome icons

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,7 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import React from 'react';
+
 import "./LandingPage.css"
 export default function LandingPage() {
     return (
