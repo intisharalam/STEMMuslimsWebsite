0206d18bfa09abcb4a6f30977643f489fea30d0c
root <root@MohamedSharif>
2022-10-27 22:18:00 +0100

    Fixed navbar so logo doesn't move about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect } from 'react'
+import React, { useState  } from 'react'
 import './Navbar.css'
 // import Container from 'react-bootstrap/Container';
 // import Navbar from 'react-bootstrap/Navbar';
@@ -10,34 +10,21 @@ import { Link } from 'react-router-dom';
 
 function NavbarHome() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+    // const [button, setButton] = useState(true);
     const handleClick  = () => setClick(!click)
     const closeMenu = () => setClick(false)
-    
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
 
-  useEffect(() => {
-    showButton();
-  }, []);
-  window.addEventListener('resize', showButton);
     return (
         <div>
             <nav className=  "navbar">
                 <div className='navbar-container'>
-                <img src = {process.env.PUBLIC_URL+"/logo.png"} alt = "Logo" className = "logo img-responsive d-inline-block align-top"/>
+                <img src = {process.env.PUBLIC_URL+"/logo.png"} alt = "Logo" className = "navbar-logo img-responsive d-inline-block align-top"/>
                     
-                    <h1 className = "title">Stem Muslims</h1>
+                    <h1 className = "navbar-title">STEM Muslims</h1>
                     <div className='menu-icon' onClick = {handleClick}>
                             <i className={click ? 'fa fas fa-times' : 'fa fas fa-bars'} ></i>
                     </div> 
-                        <ul className={click ? 'navmenu-active' : 'nav-menu'}>
-                           
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                             <li className = 'nav-item' >
                             <Link to='/' className='nav-links' onClick={closeMenu}>
                                 About Us
