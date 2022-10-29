56f0d063f5907b08c7a9129e429e455b1aea5726
AmeenIzhac <ameen.izhac@gmail.com>
2022-10-29 15:01:01 +0100

    dw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import  {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
+import CommitteePage from './CommitteePage'
 import React from 'react';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Router>
           <Routes>
             <Route path="" exact element={<LandingPage />} />
+            <Route path="/committee" exact element={<CommitteePage />} />
           </Routes>
         </Router>
       </div>
---
+++ src/CommitteePage.js
@@ -0,0 +1,7 @@
+import './CommitteePage.css'
+
+export default function CommitteePage() {
+    return (
+        <h1 className='justanexamplestyle'>this is the committee page</h1>
+    )
+}
